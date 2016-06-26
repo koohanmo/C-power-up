bb96fcc7f1b89746024162ebf27f313f073dd24a
kuhanmo <rngksah@naver.com>
2016-06-27 01:37:19 +0900

    part 10 Complete

---
+++ part9_35.h
@@ -0,0 +1,11 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H
+
+#include<stdio.h>
+#define STR_MAX 100
+
+int strLen(char *str);
+int strCpy(char* dest, char* source);
+int strCat(char* dest, char* source);
+
+#endif
